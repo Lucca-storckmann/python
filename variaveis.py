9d87b4b8d21f569ad3cb3688ac118e1ccba7ce76
Lucca Storckmann Medeiros <lucca.sto.medeiros@gmail.com>
2026-02-24 18:39:36 -0300

    Estudos

--- variaveis.py
+++ variaveis.py
@@ -1,6 +1,13 @@
-nome = "Lucca" #string (texto)
-idade = 22 #int (número inteiro)
-distancia_entre_cidades = 35.5 #float (número decimal)
-prestando_atencao = True #bool (booleano, ou seja, verdadeiro ou falso)
-texto = "Olá, mundo!" #string (texto) - pode conter letras, números e símbolos, mas é tratado como texto
+primeiro_valor = int(input("Digite aqui o primeiro valor: "))
+segundo_valor = int(input("Digite aqui o segundo valor: "))
+
+soma = primeiro_valor + segundo_valor
+print("A soma dos dois valores é: ", soma)
+subtracao = primeiro_valor - segundo_valor 
+print("A subtração dos dois valores é: ", subtracao)
+multiplicacao = primeiro_valor * segundo_valor
+print("A multiplicação dos dois valores é: ", multiplicacao)
+divisao = primeiro_valor / segundo_valor
+print("A divisão dos dois valores é: ", divisao)
+
 
