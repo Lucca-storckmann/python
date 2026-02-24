2ccac36e5bce9a94db43139fc72a52f8d3d2706c
Lucca Storckmann Medeiros <lucca.sto.medeiros@gmail.com>
2026-02-23 21:31:33 -0300

    att

---
+++ variaveis.py
@@ -0,0 +1,6 @@
+nome = "Lucca" #string (texto)
+idade = 22 #int (número inteiro)
+distancia_entre_cidades = 35.5 #float (número decimal)
+prestando_atencao = True #bool (booleano, ou seja, verdadeiro ou falso)
+texto = "Olá, mundo!" #string (texto) - pode conter letras, números e símbolos, mas é tratado como texto
+
